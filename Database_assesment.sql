fc99ad3615d8feea14acef9be709c4bb9a02570f
vipulsapra007 <vipulsapra007@gmail.com>
2023-03-04 11:33:54 +0530

    database_assessment

---
+++ Database_assesment.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 03, 2023 at 04:33 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `assesment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+CREATE TABLE `city` (
+  `city_id` int(11) NOT NULL,
+  `city_name` varchar(20) DEFAULT NULL,
+  `lat` double DEFAULT NULL,
+  `Iong` double DEFAULT NULL,
+  `country_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`city_id`, `city_name`, `lat`, `Iong`, `country_id`) VALUES
+(1, 'Berlin', 52.520008, 13.4044954, 1),
+(2, 'Belgrade', 44.787197, 20.457273, 2),
+(3, 'Zagreb', 45.815399, 15.966568, 3),
+(4, 'New York', 40.73061, -73.935242, 4),
+(5, 'Los Angeles', 34.052235, -118.243683, 4),
+(6, 'Warsaw', 52.237049, 21.017532, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE `country` (
+  `country_id` int(11) NOT NULL,
+  `country_name` varchar(20) DEFAULT NULL,
+  `country_name_eng` varchar(20) DEFAULT NULL,
+  `country_code` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`country_id`, `country_name`, `country_name_eng`, `country_code`) VALUES
+(1, 'Deutschland', 'Germany', 'DEU'),
+(2, 'Srbija', 'Serbia', 'SRB'),
+(3, 'Hrvatska', 'Croatia', 'HRV'),
+(4, 'United States of Ame', 'United States of Ame', 'USA'),
+(5, 'Polska', 'Poland', 'POL'),
+(6, 'Espana', 'Spain', 'ESP'),
+(7, 'Rossiya', 'Russia', 'RUS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL,
+  `customer_name` varchar(20) DEFAULT NULL,
+  `city_id` int(11) DEFAULT NULL,
+  `customer_address` varchar(20) DEFAULT NULL,
+  `next_call_date` varchar(20) DEFAULT NULL,
+  `ts_inserted` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`customer_id`, `customer_name`, `city_id`, `customer_address`, `next_call_date`, `ts_inserted`) VALUES
+(1, 'Jewelry Store', 4, 'Long Street 120', '2020-01-21', '2020-01-09[14:01:20.000]'),
+(2, 'Bakery', 1, 'Kurfurstendamm 25', '2020-01-21', '2020-01-09[17:52:15.000]'),
+(3, 'Cafe', 1, 'Tauentzienstrabe 44', '2020-01-21', '2020-01-10[08:02:49.000]'),
+(4, 'Restaurant', 3, 'Ulica Lipa 15', '2020-01-21', '2020-01-10[09:20:21.000]');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`city_id`),
+  ADD KEY `country_id` (`country_id`);
+
+--
+-- Indexes for table `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`country_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`),
+  ADD KEY `city_id` (`city_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `country`
+--
+ALTER TABLE `country`
+  MODIFY `country_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `city`
+--
+ALTER TABLE `city`
+  ADD CONSTRAINT `city_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`);
+
+--
+-- Constraints for table `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
